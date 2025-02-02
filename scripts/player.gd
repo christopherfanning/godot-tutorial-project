52aac4584373c19dfab7a2e5a083de62d6808cef
Chris Fanning <chris@cfan.dev>
2025-02-02 13:01:13 -0600

    Basic player movement
    Player Acceleration
    Add h_flip for direction
    Add animations for jump and walk

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,9 +1,10 @@
 extends CharacterBody2D
 
 
-const SPEED: float         = 200.0
+const SPEED: float = 200.0
 const JUMP_VELOCITY: float = -300.0
-
+const ACCELERATION: float = 800.0
+const FRICTION: float = 600.0
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -11,18 +12,19 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if (Input.is_action_just_pressed("gamepad_jump") or 
+	if (Input.is_action_just_pressed("gamepad_jump") or
 		Input.is_action_just_pressed("ui_accept")) and is_on_floor():
-		
 		velocity.y = JUMP_VELOCITY
-		
-		
+		$AnimatedSprite2D.play("jumping")
+
 	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("ui_left", "ui_right")
 	if direction:
-		velocity.x = direction * SPEED
+		velocity.x = move_toward(velocity.x, direction * SPEED, ACCELERATION * delta)
+		$AnimatedSprite2D.play("walking")
+		$AnimatedSprite2D.flip_h = direction < 0
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, FRICTION * delta)
+		$AnimatedSprite2D.play("idle")
 
 	move_and_slide()
